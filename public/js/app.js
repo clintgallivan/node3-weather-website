7ee95795efeb1a9133ea2a5165f030c51937ce2c
Clinton Gallivan <clintongallivan@clintons-mbp.attlocal.net>
2021-01-05 17:43:40 -0800

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+console.log("Client side javascript file is loaded!");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then(({ error, location, forecast }) => {
+        if (error) {
+          return (
+            (messageOne.textContent = error), (messageTwo.textContent = "")
+          );
+        }
+        messageOne.textContent = location;
+        messageTwo.textContent = forecast;
+      });
+    }
+  );
+});
+
+// fetch(`http://localhost:3000/weather?address=${location}`).then(
+//     (response) => {
+//       response.json().then(({ error, location, forecast }) => {
+//         if (error) {
+//           return console.log(error);
+//         }
+//         console.log(location);
+//         console.log(forecast);
+//       });
+//     }
+//   );
---
+++ readme.txt
@@ -0,0 +1,3 @@
+open the file by running in the command line: node (or nodemon) app.js (in the src folder)
+
+runs on port localhost:3000
